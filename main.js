215bc810188785d950b76504104925fd52abb4fa
NovelTime <105994773+NovelTime@users.noreply.github.com>
2022-06-05 21:12:42 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,15 @@ var novel = [
     sourceLink: "series/The-Neighboring-Aaryasan-who-Sometimes-Acts-Affectionate-and-Murmuring-in-Russian.html"
   },
   {
+    title: "That Time I Got Reincarnated as a Slime",
+    sourceLink: "series/tensei-shittara-slime.html"
+  },
+  {
     title: "How Could You Like Another Girl When You Already Have A Cute Fiancée Like Me?",
     sourceLink: "series/How-Could-You-Like-Another-Girl-When-You-Already-Have-A-Cute-Fiance-Like-Me.html"
+  },
+  {
+    title: "A Gal Who Looks Good in an Apron is Unfair!",
+    sourceLink: "series/aprongal.html"
   }
-  ];
+];
