c5f02a667071d04fb29f47426d13ebb4e74756b7
NovelTime <105994773+NovelTime@users.noreply.github.com>
2022-06-05 17:26:37 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var novel = [
+  
+  {
+    title: "The Neighboring Aarya-san who Sometimes Acts Affectionate and Murmuring in Russian",
+    sourceLink: "series/The-Neighboring-Aaryasan-who-Sometimes-Acts-Affectionate-and-Murmuring-in-Russian.html"
+  },
+  {
+    title: "How Could You Like Another Girl When You Already Have A Cute Fiancée Like Me?",
+    sourceLink: "series/How-Could-You-Like-Another-Girl-When-You-Already-Have-A-Cute-Fiance-Like-Me.html"
+  }
+  ];
