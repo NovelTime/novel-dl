5f9a7c8e39d36291169e57290ff8e1081f6b7f11
NovelTime <105994773+NovelTime@users.noreply.github.com>
2022-06-05 23:10:48 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var novel = [
   
   {
+    title: "Eromanga Sensei",
+    sourceLink: "series/eromanga.html"
+  },
+  {
     title: "The Neighboring Aarya-san who Sometimes Acts Affectionate and Murmuring in Russian",
     sourceLink: "series/The-Neighboring-Aaryasan-who-Sometimes-Acts-Affectionate-and-Murmuring-in-Russian.html"
   },
@@ -9,15 +13,15 @@ var novel = [
     sourceLink: "series/tensei-shittara-slime.html"
   },
   {
+    title: "After I Lectured an Unfriendly Girl Who Doesn\'t Let Strangers Approach her, She Really Took a Liking to Me",
+    sourceLink: "series/tanin-wo-yosetsukenai-buaisouna-joshi-ni-sekkyou-shitara-mechakucha-natsukareta.html"
+  },
+  {
     title: "How Could You Like Another Girl When You Already Have A Cute Fiancée Like Me?",
     sourceLink: "series/How-Could-You-Like-Another-Girl-When-You-Already-Have-A-Cute-Fiance-Like-Me.html"
   },
   {
     title: "A Gal Who Looks Good in an Apron is Unfair!",
     sourceLink: "series/aprongal.html"
-  },
-  {
-    title: "After I Lectured an Unfriendly Girl Who Doesn't Let Strangers Approach her, She Really Took a Liking to Me",
-    sourceLink: "series/tanin-wo-yosetsukenai-buaisouna-joshi-ni-sekkyou-shitara-mechakucha-natsukareta.html"
   }
 ];
